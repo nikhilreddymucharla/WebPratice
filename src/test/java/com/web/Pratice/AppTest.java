47016c73356b7ebb98cf8f2470dbdfd6b09e0af2
HP <HP@DESKTOP-RF2NQC6>
2021-01-14 17:50:43 +0530

    third changes

--- src/test/java/com/web/Pratice/AppTest.java
+++ src/test/java/com/web/Pratice/AppTest.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
@@ -21,13 +24,15 @@ public class AppTest {
     public void setUp() throws MalformedURLException {
         DesiredCapabilities capability = DesiredCapabilities.chrome();
         driver = new RemoteWebDriver(new URL("http://selenium-ch:4444/wd/hub"), capability);
+        driver.get("https://google.com");
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
     }
 	@org.testng.annotations.Test
 	public void m1() throws MalformedURLException {
 		
-		driver.get("https://google.com");
+		WebDriverWait wait = new WebDriverWait(driver,30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@title='Search']")));
 		driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("HI google");
 		driver.findElement(By.xpath("(//input[@value='Google Search' and @aria-label='Google Search'])[2]")).click();
 		
