4fa67a60d73187fac57c5563f10d09b86664931c
HP <HP@DESKTOP-RF2NQC6>
2021-01-15 06:43:48 +0530

    seetest intigrations

--- src/test/java/com/web/Pratice/AppTest.java
+++ src/test/java/com/web/Pratice/AppTest.java
@@ -5,46 +5,55 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-	
-	private WebDriver driver;
 
-    @BeforeSuite
-    public void setUp() throws MalformedURLException {
-        DesiredCapabilities capability = DesiredCapabilities.chrome();
-        driver = new RemoteWebDriver(new URL("http://selenium-ch:4444/wd/hub"), capability);
-        driver.get("https://google.com");
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-    }
-	@org.testng.annotations.Test
-	public void m1() throws MalformedURLException {
-		
-		//WebDriverWait wait = new WebDriverWait(driver,30);
-		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@title='Search']")));
-		//driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("HI google");
-		driver.findElement(By.xpath("(//input[@value='Google Search' and @aria-label='Google Search'])[2]")).click();
-		
-		System.out.println("The Test cases are executed******************************");
+	private static final String ACCESS_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJ4cC51Ijo4MDkwNTUzLCJ4cC5wIjo4MDExNTgyLCJ4cC5tIjoxNjA3MTc2NDcwNDAzLCJleHAiOjE5MjI2MzA0MjEsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.3L36xT8cQv-ZIHMalFGfTEXEJj4qY3Q-QGOuYVqWw_k";
+	private RemoteWebDriver driver;
+	private URL url;
+	private DesiredCapabilities dc = new DesiredCapabilities();
 
+	@BeforeTest
+	public void setUp() throws Exception {
+		dc.setCapability("testName", "Quick Start Chrome Browser Demo");
+		dc.setCapability("accessKey", ACCESS_KEY);
+		dc.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+		driver = new RemoteWebDriver(new URL("https://cloud.seetest.io/wd/hub"), dc);
 	}
 
-	@AfterSuite
-	public void afterTest() {
-		if (driver != null) {
-			driver.quit();
+	@Test
+	public void browserTestGoogleSearch() {
+		for (int i = 0; i < 7; i++) {
+			driver.get("https://www.google.com");
+			new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.name("q")));
+			WebElement searchBar = driver.findElement(By.name("q"));
+			searchBar.click();
+			searchBar.sendKeys("Experitest");
+			searchBar.sendKeys(Keys.ENTER);
 		}
 	}
+
+	@AfterTest
+	public void tearDown() {
+		System.out.println("Report URL: " + driver.getCapabilities().getCapability("reportUrl"));
+		driver.quit();
+	}
+
 }
