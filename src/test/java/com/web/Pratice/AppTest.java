744a2d733415984960a7f450055806c0f4773ae6
HP <HP@DESKTOP-RF2NQC6>
2021-01-16 15:43:42 +0530

    done some changes

--- src/test/java/com/web/Pratice/AppTest.java
+++ src/test/java/com/web/Pratice/AppTest.java
@@ -30,14 +30,14 @@ public class AppTest {
 	private static final String ACCESS_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJ4cC51Ijo4MDkwNTUzLCJ4cC5wIjo4MDExNTgyLCJ4cC5tIjoxNjA3MTc2NDcwNDAzLCJleHAiOjE5MjI2MzA0MjEsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.3L36xT8cQv-ZIHMalFGfTEXEJj4qY3Q-QGOuYVqWw_k";
 	private RemoteWebDriver driver;
 	private URL url;
-	private DesiredCapabilities dc = new DesiredCapabilities().
+	private DesiredCapabilities dc = new DesiredCapabilities();
 
 	@BeforeTest
 	public void setUp() throws Exception {
 		//dc.setCapability("testName", "Quick Start Chrome Browser Demo");
 		//dc.setCapability("accessKey", ACCESS_KEY);
 		dc.setCapability(CapabilityType.BROWSER_NAME, "chrome");
-		dc.setCapability(CapabilityType.PLATFORM_NAME, "Windows 10");	
+		dc.setCapability(CapabilityType.PLATFORM, "LINUX");	
 		//driver = new RemoteWebDriver(new URL("https://cloud.seetest.io/wd/hub"), dc);
 		driver = new RemoteWebDriver(new URL("http://selenium-ch:4444/wd/hub"), dc);
 		driver.get("https://qe.acto.com/auth/sso");
